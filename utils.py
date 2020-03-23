3de5856d6099246b2008ddec23395294d536e147
Qi Li <q.l.lawliet@gmail.com>
2020-03-23 18:39:35 -0400

    titanic simple model

--- utils.py
+++ utils.py
@@ -25,3 +25,26 @@ def load_df(fpath):
     n, d = df.shape
     print("{} rows and {} columns read from {}".format(n, d, fpath))
     return df
+
+def get_df_info(df):
+    """
+    print several info for a pandas df
+    """
+    null_count = df.isnull().astype('int').sum(axis=0) # number of nulls for each col
+    print("-"*20 + "missing values" + "-"*20)
+    for col in df.columns:
+        if null_count[col] != 0:
+            print(col, null_count[col])
+    str_cols = []
+    num_cols = []
+    for col in df.columns:
+        if df[col].dtype == np.dtype('O'):
+            str_cols.append(col)
+        elif df[col].dtype == np.dtype('float') or df[col].dtype == np.dtype('int'):
+            num_cols.append(col)
+    print("-"*20 + "numeric columns" + "-"*20)
+    print(num_cols)
+    print("-"*20 + "string columns" + "-"*20)
+    print(str_cols)
+    print("-"*20 + "statistics" + "-"*20)
+    print(df[num_cols].describe())
